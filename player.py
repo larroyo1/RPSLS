722c8e1a98eb81b016bf34045af0505d35f19a1e
larroyo1 <arroyoluis1516@yahoo.com>
2022-04-06 10:17:47 -0600

    add methods to player class

--- player.py
+++ player.py
@@ -3,9 +3,12 @@ class Player:
         self.name = name
         self.score = 0
         self.gestures = ["Rock","Paper","Scissors","Lizard","Spock"]
+        self.current_gesture = ''
+        
+     def choose_gesture(self):
+        for gesture in self.gestures:
+           print(f'{self.gestures.index(gesture)}: {gesture}')
 
-    def throw_gesture(self):
-        pass
-
-    def choose_gesutre(self):
-        pass
+        user_input = int(input('Choose a gesture by number.'))
+        self.current_gesture = self.gestures[user_input]
+       
