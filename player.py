4a3245d02a8a4a3f226cbff3cd06e63f70e06776
alex-colvin <alexcolvin@live.com>
2022-04-06 13:06:30 -0400

    working with Player.choose_gesture input validation.

--- player.py
+++ player.py
@@ -1,14 +1,24 @@
+#reset self.current_gesture after each turn...
 class Player:
-    def __init__(self, name):
-        self.name = name
-        self.score = 0
-        self.gestures = ["Rock","Paper","Scissors","Lizard","Spock"]
-        self.current_gesture = ''
+   def __init__(self, name):
+      self.name = name
+      self.score = 0
+      self.gestures = ["Rock","Paper","Scissors","Lizard","Spock"]
+      self.current_gesture = None
         
-     def choose_gesture(self):
-        for gesture in self.gestures:
-           print(f'{self.gestures.index(gesture)}: {gesture}')
+   def choose_gesture(self):
+      for gesture in self.gestures:
+         print(f'{self.gestures.index(gesture)}: {gesture}')
 
-        user_input = int(input('Choose a gesture by number.'))
-        self.current_gesture = self.gestures[user_input]
+      user_input = input('Choose a gesture by number :')
+      #;need to check if the user enters a letter...
+      for gesture in self.gestures:
+         index = self.gestures.index(gesture)
+         if int(user_input) == index:
+            self.current_gesture = self.gestures[int(user_input)]
+            print(f"You chose {self.gestures[int(user_input)]}")
+            break
+      if self.current_gesture == None:
+         print("That is not a valid repsonse.")
+         self.choose_gesture()
        
